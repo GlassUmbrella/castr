01fbcc23db61c9ea7b52b989df4ba0d6619ae1c7
Weiran Zhang <weiran@zhang.me.uk>
2013-05-22 23:26:26 +0100

    Switched app to use bliss.

--- app.js
+++ app.js
@@ -7,20 +7,27 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , Bliss = require('bliss');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+
+// // set view engine
+var bliss = new Bliss();
+app.engine("html", function(path, options, fn) {
+	fn(null, bliss.render(path, options));
+});
 
 // development only
 if ('development' == app.get('env')) {
@@ -29,6 +36,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
