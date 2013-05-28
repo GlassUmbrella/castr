1e0f1d133d973b1aabb0ee70cf4e018f6448adf5
Weiran Zhang <weiran@zhang.me.uk>
2013-05-28 11:24:54 +0100

    Removed debug code for mailer.

--- controllers/dashboard-controller.js
+++ controllers/dashboard-controller.js
@@ -1,13 +1,4 @@
 exports.index = function(req, res) {
-	var mailer = require("../lib/mailer");
-	mailer.sendMail({
-		from: "Castr Team <team@castr.net>", // sender address
-	    to: "weiran@zhang.me.uk", // list of receivers
-	    subject: "Hello ✔", // Subject line
-	    text: "Hello world ✔", // plaintext body
-	    html: "<b>Hello world ✔</b>" // html body
-	});
-	
 	res.render("dashboard", {
 		title: "Dashboard",
 		user: req.session.user,
