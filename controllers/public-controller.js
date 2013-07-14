b22b0abd96335c743f3db32f9c21d566e22d8b70
Weiran Zhang <weiran@zhang.me.uk>
2013-07-14 15:45:44 +0100

    Some initial homepage blurb.

--- controllers/public-controller.js
+++ controllers/public-controller.js
@@ -1,7 +1,7 @@
 exports.home = function(req, res) {
-	res.render("public/index", { title: "Show your podcast to the world." });
+	res.render("public/index", { title: "Podcast to the world" });
 };
 
 exports.about = function(req, res) {
-	res.render("public/about", { title: "Some text about us" });
+	res.render("public/about", { title: "About" });
 };
