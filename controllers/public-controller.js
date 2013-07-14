12fa3513bc235e2c2d4a783e54b55e582d68e5fc
Weiran Zhang <weiran@zhang.me.uk>
2013-07-14 17:45:04 +0100

    Redirect logged in user from home page to dashboard.

--- controllers/public-controller.js
+++ controllers/public-controller.js
@@ -1,5 +1,9 @@
 exports.home = function(req, res) {
-	res.render("public/index", { title: "Podcast to the world" });
+	if (!req.session.user) {
+		res.render("public/index", { title: "Podcast to the world" });
+	} else {
+		res.redirect("/dashboard");
+	}
 };
 
 exports.about = function(req, res) {
