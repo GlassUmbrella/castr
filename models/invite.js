a72bf96de783abb3e37354e5482943888a1998cd
Eddie Lee <dudelee@gmail.com>
2013-07-14 22:24:33 +0100

    Added invite model and altered how table options are handled.

---
+++ models/invite.js
@@ -0,0 +1,41 @@
+//Getting the orm instance
+var orm = require("../lib/model")
+  , Seq = orm.Seq();
+
+//Creating our module
+module.exports = {
+	model: {
+		emailAddress: {
+			type: Seq.STRING,
+			unique: true,
+            allowNull: false
+		},
+		name: {
+			type: Seq.STRING,
+			allowNull: false
+		},
+		inviteCode: {
+			type: Seq.STRING,
+			allowNull: false
+		},
+		dateRequested: {
+			type: Seq.DATE,
+			allowNull: false
+		},
+		dateSent: {
+			type: Seq.DATE,
+			allowNull: true
+		},
+		dateActivated: {
+			type: Seq.DATE,
+			allowNull: true
+		},
+		createdUserId: {
+			type: Seq.INTEGER,
+			allowNull: true
+		}
+	},
+	options: {
+		timestamps: false
+	}
+}
