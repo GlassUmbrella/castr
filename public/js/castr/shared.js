e5d5ba5df0ecbf42b4d0393b1dca77b7c3f7bc4f
Weiran Zhang <weiran@zhang.me.uk>
2013-07-20 13:36:09 +0100

    Date parsing for podcasts page. Episode type selector.

--- public/js/castr/shared.js
+++ public/js/castr/shared.js
@@ -1,4 +1,4 @@
-// js implementation of .NET String.Format
+// js implementation of .NET String methods
 if(!String.prototype.format) {
 	String.prototype.format = function() {
 	    var args = arguments;
@@ -24,4 +24,35 @@ if(!String.prototype.replaceAll) {
 	String.prototype.replaceAll = function (find, replace) {
 	    return this.replace(new RegExp(find, 'g'), replace);
 	};
+}
+
+// Implementation of Array.filter for old browsers
+if (!Array.prototype.filter)
+{
+  Array.prototype.filter = function(fun /*, thisp*/)
+  {
+    "use strict";
+
+    if (this == null)
+      throw new TypeError();
+
+    var t = Object(this);
+    var len = t.length >>> 0;
+    if (typeof fun != "function")
+      throw new TypeError();
+
+    var res = [];
+    var thisp = arguments[1];
+    for (var i = 0; i < len; i++)
+    {
+      if (i in t)
+      {
+        var val = t[i]; // in case fun mutates this
+        if (fun.call(thisp, val, i, t))
+          res.push(val);
+      }
+    }
+
+    return res;
+  };
 }
