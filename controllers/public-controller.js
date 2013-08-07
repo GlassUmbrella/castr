ddfa5258a665d82c6081a00ddc9297330901498b
Weiran Zhang <weiran@zhang.me.uk>
2013-08-07 11:45:54 +0100

    Removed feed for v1.

--- controllers/public-controller.js
+++ controllers/public-controller.js
@@ -2,7 +2,7 @@ exports.home = function(req, res, next) {
 	if (!req.session.user) {
 		res.render("public/index", { title: "Podcast to the world" });
 	} else {
-		res.redirect("/feed");
+		res.redirect("/podcasts");
 	}
 };
 
