0b03c9af236db5dae4b8abaf157512a2fecce955
Weiran Zhang <weiran@zhang.me.uk>
2014-03-12 22:56:53 +0000

    Added example config.js.

---
+++ config-example.js
@@ -0,0 +1,29 @@
+this.mailer = {
+	service: "Mandrill",
+    auth: {
+        user: "",
+        pass: ""
+    }
+};
+
+this.mysql = {
+	dev: {
+		username: "",
+		password: "",
+		database: "",
+		host: ""
+	},
+	live: {
+		username: "",
+		password: "",
+		database: "",
+		host: ""
+	}
+};
+
+this.twitter = {
+	consumerKey: "",
+	consumerSecret: "",
+	accessTokenKey: "",
+	accessTokenSecret: ""
+};
